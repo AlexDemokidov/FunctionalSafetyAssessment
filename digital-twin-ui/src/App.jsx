1914aeae43577dac01f2cea1b4ba3c559515cc2a
AlexDemokidov <alex@DESKTOP-76F7GVQ>
2025-04-20 22:04:27 +0300

    Change ui

--- digital-twin-ui/src/App.jsx
+++ digital-twin-ui/src/App.jsx
@@ -6,6 +6,7 @@ import Header from "./components/Header.jsx";
 import Projects from "./components/Projects.jsx";
 import Measure from "./components/Measure.jsx";
 import Modal from "./components/Modal.jsx";
+import Main from './components/Main.jsx';
 
 const App = () => {
 
@@ -16,14 +17,6 @@ const App = () => {
   const [projectId, setProjectId] = useState("")
   const [projectData, setProjectData] = useState(null)
   const [projectFailureRate, setProjectFailureRate] = useState(null)
-
-  const openModal = () => {
-    setModalStyle({ display: 'block' });
-  };
-
-  const closeModal = () => {
-    setModalStyle({ display: 'none' });
-  };
 
   const closeProject = () => {
     setProjectId("");
@@ -93,120 +86,6 @@ const App = () => {
     }
   }
 
-  const createProject = (e) => {
-    // event.preventDefault();
-    console.log(fileText);
-
-    let lines = fileText.split('\n');
-
-    if (lines[0].includes('pdsprj')) {
-      lines.shift(); // Remove file path
-    } else {
-      lines.shift(); // Remove file path
-      lines.pop(); // Remove .end
-      lines.pop(); // Remove .backanno
-      lines.pop(); // Remove .op
-      lines.pop(); // Remove .op
-    }
-
-    for (let i = 0; i < lines.length; i++) {
-      lines[i] = lines[i].replace('BAT', 'V');
-    }
-
-    console.log(lines)
-
-    const simpleComponents = ['R', 'V', 'I', 'C'];
-
-    const netlist = [];
-    let res_dict = {};
-
-    for (const line of lines) {
-      const parameters = line.split(' ');
-      const type = parameters[0][0];
-      console.log(type)
-      if (simpleComponents.includes(type)) {
-        res_dict = {
-          name: parameters[0],
-          type: "-",
-          node1: parameters[1],
-          node2: parameters[2],
-          value: parameters[3],
-          node3: "-",
-          node4: "-",
-          voltage: "-",
-          current: "-",
-          power: "-",
-          failure_rate: "",
-          mtbf: ""
-        };
-        console.log(res_dict)
-
-        if (parameters[3].includes('K') || parameters[3].includes('k')) {
-          res_dict.value = parseFloat(parameters[3].slice(0, -1));
-          res_dict.value *= 1000;
-          res_dict.value = res_dict.value.toString()
-        } else if (parameters[3].includes('U') || parameters[3].includes('u')) {
-          res_dict.value = parseFloat(parameters[3].slice(0, -1));
-          res_dict.value /= 1000000;
-          res_dict.value = res_dict.value.toString()
-        } else if (parameters[3].includes('V')) {
-          res_dict.value = parseFloat(parameters[3].slice(0, -1));
-          res_dict.value = res_dict.value.toString()
-        }
-      } else if (type === 'Q') {
-        res_dict = {
-          name: parameters[0],
-          node1: parameters[1],
-          node2: parameters[2],
-          node3: parameters[3],
-          value: parameters[4],
-          type: "-",
-          node4: "-",
-          voltage: "-",
-          current: "-",
-          power: "-",
-          failure_rate: "",
-          mtbf: ""
-        };
-      } else if (type === 'D') {
-        res_dict = {
-          name: parameters[0],
-          node1: parameters[1],
-          node2: parameters[2],
-          value: parameters[3],
-          type: "-",
-          node3: "-",
-          node4: "-",
-          voltage: "-",
-          current: "-",
-          power: "-",
-          failure_rate: "",
-          mtbf: ""
-        };
-      }
-      netlist.push(res_dict);
-    }
-    console.log('project: ', netlist);
-
-    let project = netlist
-
-    console.log(project)
-
-    const response = fetch(`http://localhost:8000/projects`, {
-      method: "POST",
-      headers: { "Accept": "application/json", "Content-Type": "application/json" },
-      body: JSON.stringify({
-        project
-      })
-    });
-    if (response.ok) {
-      window.location.reload()
-      console.log(response);
-    }
-    else
-      console.log(response);
-  };
-
   const measure = (e) => {
     let project = projectData.project;
     console.log(project);
@@ -229,18 +108,24 @@ const App = () => {
 
   };
 
-  return projectId === "" ? (
-    <div className='projects'>
-      <Header openModal={openModal} />
-      <Modal modalStyle={modalStyle} setFileText={setFileText} fileText={fileText} createProject={createProject} closeModal={closeModal} />
-      {projects ? <Projects projects={projects} onClick={onClick} deleteProject={deleteProject} /> : <Spin size="large" />}
-    </div>
-  ) : (
-    <div>
-      <Header openModal={openModal} />
-      <Modal modalStyle={modalStyle} setFileText={setFileText} fileText={fileText} createProject={createProject} closeModal={closeModal} />
-      {projectData ? <Measure project={projectData} measure={measure} setProjectData={setProjectData} projectFailureRate={projectFailureRate} closeProject={closeProject} /> : <Spin size="large" />}
-    </div >
+  // return projectId === "" ? (
+  //   <div className='projects'>
+  //     <Header openModal={openModal} />
+  //     <Modal modalStyle={modalStyle} setFileText={setFileText} fileText={fileText} createProject={createProject} closeModal={closeModal} />
+  //     {projects ? <Projects projects={projects} onClick={onClick} deleteProject={deleteProject} /> : <Spin size="large" />}
+  //   </div>
+  // ) : (
+  //   <div>
+  //     <Header openModal={openModal} />
+  //     <Modal modalStyle={modalStyle} setFileText={setFileText} fileText={fileText} createProject={createProject} closeModal={closeModal} />
+  //     {projectData ? <Measure project={projectData} measure={measure} setProjectData={setProjectData} projectFailureRate={projectFailureRate} closeProject={closeProject} /> : <Spin size="large" />}
+  //   </div >
+  // );
+
+  return (
+    <>
+      {projects ? <Main projects={projects} setProjectId={setProjectId} measure={measure} setProjectData={setProjectData} /> : <Spin size="large" />}
+    </>
   );
 
 };
---
+++ digital-twin-ui/src/components/Main.jsx
@@ -0,0 +1,133 @@
+import React, { useState } from 'react';
+import {
+    DesktopOutlined,
+    FileOutlined,
+    PieChartOutlined,
+    TeamOutlined,
+    HomeOutlined,
+} from '@ant-design/icons';
+import { Breadcrumb, Layout, Menu, theme } from 'antd';
+import ProjectCreation from './ProjectCreation';
+import About from './About';
+import Measure from './Measure';
+
+const { Header, Content, Footer, Sider } = Layout;
+function getItem(label, key, icon, children) {
+    return {
+        key,
+        icon,
+        children,
+        label,
+    };
+}
+
+
+function Main(props) {
+
+    let { projects } = props;
+
+    const [selectedMenuItem, setSelectedMenuItem] = useState(1);
+
+    const handleMenuClick = (e) => {
+        console.log('Clicked item:', e.key);
+
+        // Можно сохранить выбранный ключ в состоянии
+        setSelectedMenuItem(e.key);
+
+        // Если keyPath содержит больше одного элемента — это вложенный пункт
+        if (e.keyPath[1] == "projects") {
+            setSelectedMenuItem(3);
+
+            // console.log(e.key)
+            props.setProjectId(e.key)
+            console.log('Это вложенный пункт, родитель:', e.keyPath[1]);
+        }
+    };
+
+    console.log(projects)
+
+    const items = [
+        getItem('Главная', '1', <HomeOutlined />),
+        getItem('Создать проект', '2', <PieChartOutlined />),
+        getItem('Проекты', 'projects', <DesktopOutlined />, projects.projects.map(project => (
+            getItem(project.name, project.id))
+        )),
+        getItem('Контакты', '4', <TeamOutlined />),
+    ];
+
+    const [collapsed, setCollapsed] = useState(false);
+    const {
+        token: { colorBgContainer, borderRadiusLG },
+    } = theme.useToken();
+
+    return (
+        <Layout style={{ minHeight: '100vh' }}>
+            <Sider collapsible collapsed={collapsed} onCollapse={value => setCollapsed(value)}>
+                <div className="demo-logo-vertical m-5 grid justify-items-center">
+                    <img className='w-15' src="./src/img/fsa-logo-1.png" alt="" />
+                </div>
+                <Menu theme="dark" defaultSelectedKeys={['1']} mode="inline" items={items} onClick={handleMenuClick} />
+            </Sider>
+            <Layout>
+                {/* <Header style={{ padding: 0, background: colorBgContainer }} /> */}
+                <Content style={{ margin: '0 16px' }}>
+                    {selectedMenuItem == 1 &&
+                        <>
+                            <Breadcrumb style={{ margin: '16px 0' }}>
+                                <Breadcrumb.Item>Главная</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <div
+                                style={{
+                                    padding: 24,
+                                    minHeight: 360,
+                                    background: colorBgContainer,
+                                    borderRadius: borderRadiusLG,
+                                }}
+                            >
+                                <About />
+                            </div>
+                        </>
+                    }
+                    {selectedMenuItem == 2 &&
+                        <>
+                            <Breadcrumb style={{ margin: '16px 0' }}>
+                                <Breadcrumb.Item>Создать проект</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <div
+                                style={{
+                                    padding: 24,
+                                    minHeight: 360,
+                                    background: colorBgContainer,
+                                    borderRadius: borderRadiusLG,
+                                }}
+                            >
+                                <ProjectCreation />
+                            </div>
+                        </>
+                    }
+                    {selectedMenuItem == 3 &&
+                        <>
+                            <Breadcrumb style={{ margin: '16px 0' }}>
+                                <Breadcrumb.Item>Проект</Breadcrumb.Item>
+                            </Breadcrumb>
+                            <div
+                                style={{
+                                    padding: 24,
+                                    minHeight: 360,
+                                    background: colorBgContainer,
+                                    borderRadius: borderRadiusLG,
+                                }}
+                            >
+                                <Measure project={projectData} />
+                            </div>
+                        </>
+                    }
+                </Content>
+                <Footer style={{ textAlign: 'center' }}>
+                    Alex Demokidov Design ©{new Date().getFullYear()} Created by Alex Demokidov
+                </Footer>
+            </Layout>
+        </Layout>
+    );
+};
+export default Main;
